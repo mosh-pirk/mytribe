ccc97e172720c1007bbe1d70bf01a237e45e81f1
mosh-pirk <106318369+mosh-pirk@users.noreply.github.com>
2023-06-26 18:54:32 +0300

    Project setup

---
+++ ui/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+          moi
+      </header>
+    </div>
+  );
+}
+
+export default App;
